a53837d053092480387e9256ba644df9be316eab
Pablo Sanchez <pablo@blueoakdb.com>
2023-02-03 12:04:57 -0500

    o Added log_msg() and used to provide time stamped/file name, messages.   Matches openvpn. o With little CPU cost, make availability sooner by reducing tun0, poll  sleep from 1 to 0.5 o Standardize shell variables to be uppercase. o Tacked 'exit $?' at the end, though never reached.  :)

--- data/scripts/run-socks-proxy.sh
+++ data/scripts/run-socks-proxy.sh
@@ -2,25 +2,35 @@
 
 set -e
 
-until ip link show tun0 2>&1 | grep -qv "does not exist"; do
-    sleep 1
-done
+BASENAME=$(basename "$0")
+log_msg() {
+   echo -e "$(date +'%F %T')" ["$BASENAME"] "$@"
+}
 
-proxy_config_file=config/socks-proxy.conf
+log_msg "Waiting for tun0 to become available ..."
+until ip link show tun0 2>&1 | grep -qv "does not exist"; do
+    sleep 0.5
+done
+log_msg "tun0 ready."
+
+PROXY_CONFIG_FILE=config/socks-proxy.conf
 
 if [[ $SOCKS_LISTEN_ON ]]; then
-    sed -i "/internal: /c internal: $SOCKS_LISTEN_ON port = 1080" $proxy_config_file
+    sed -i "/internal: /c internal: $SOCKS_LISTEN_ON port = 1080" $PROXY_CONFIG_FILE
 fi
 if [[ $SOCKS_PROXY_USERNAME && $SOCKS_PROXY_PASSWORD ]]; then
-    printf 'info: starting socks proxy with credentials\n'
-    id "$SOCKS_PROXY_USERNAME" &> /dev/null || useradd "$SOCKS_PROXY_USERNAME" -s /bin/false -M -p "$(mkpasswd "$SOCKS_PROXY_PASSWORD")"
-    sed -i "/method: /c method: username" $proxy_config_file
+    log_msg 'info: starting socks proxy with credentials'
+    useradd "$SOCKS_PROXY_USERNAME" -s /bin/false -M -p "$(mkpasswd "$SOCKS_PROXY_PASSWORD")"
+    sed -i "/method: /c method: username" $PROXY_CONFIG_FILE
 elif [[ -f "/run/secrets/$SOCKS_PROXY_USERNAME_SECRET" && -f "/run/secrets/$SOCKS_PROXY_PASSWORD_SECRET" ]]; then
-    printf 'info: starting socks proxy with credentials\n'
-    id "$(cat /run/secrets/"$SOCKS_PROXY_USERNAME_SECRET")" &> /dev/null || useradd "$(cat /run/secrets/"$SOCKS_PROXY_USERNAME_SECRET")" -s /bin/false -M -p "$(mkpasswd "$(cat /run/secrets/"$SOCKS_PROXY_PASSWORD_SECRET")")"
-    sed -i "/method: /c method: username" $proxy_config_file
+    log_msg 'info: starting socks proxy with credentials'
+    useradd "$(cat /run/secrets/"$SOCKS_PROXY_USERNAME_SECRET")" -s /bin/false -M -p "$(mkpasswd "$(cat /run/secrets/"$SOCKS_PROXY_PASSWORD_SECRET")")"
+    sed -i "/method: /c method: username" $PROXY_CONFIG_FILE
 else
-    printf 'info: starting socks proxy without credentials\n'
+    log_msg 'info: starting socks proxy without credentials'
 fi
 
-exec sockd -f $proxy_config_file
+exec sockd -f $PROXY_CONFIG_FILE
+
+# Never executed
+exit $?
