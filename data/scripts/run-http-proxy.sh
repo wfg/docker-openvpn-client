0fccacfdc8ffa68f4f41c640ea1f7fd561be434e
Pablo Sanchez <pablo@blueoakdb.com>
2023-02-03 11:50:26 -0500

    o Added log_msg() and used to provide time stamped/file name, messages.   Matches openvpn. o With little CPU cost, make availability sooner by reducing tun0, poll  sleep from 1 to 0.5 o Standardize shell variables to be uppercase. o Tacked 'exit $?' at the end, though never reached.  :)

--- data/scripts/run-http-proxy.sh
+++ data/scripts/run-http-proxy.sh
@@ -2,29 +2,39 @@
 
 set -e
 
+BASENAME=$(basename "$0")
+log_msg() {
+   echo -e "$(date +'%F %T')" ["$BASENAME"] "$@"
+}
+
+log_msg "Waiting for tun0 to become available ..."
 until ip link show tun0 2>&1 | grep -qv "does not exist"; do
-    sleep 1
+    sleep 0.5
 done
+log_msg "tun0 ready."
 
-proxy_config_file=config/http-proxy.conf
+PROXY_CONFIG_FILE=config/http-proxy.conf
 
-addr_eth0=$(ip address show eth0 | grep 'inet ' | awk '{split($2, inet, "/"); print inet[1]}')
-addr_tun0=$(ip address show tun0 | grep 'inet ' | awk '{split($2, inet, "/"); print inet[1]}')
+ADDR_ETH0=$(ip address show "${TAP:-eth0}" | grep 'inet ' | awk '{split($2, inet, "/"); print inet[1]}')
+ADDR_TUN0=$(ip address show tun0 | grep 'inet ' | awk '{split($2, inet, "/"); print inet[1]}')
 sed -i \
-    -e "/Listen/c Listen $addr_eth0" \
-    -e "/Bind/c Bind $addr_tun0" \
-    $proxy_config_file
+    -e "/Listen/c Listen $ADDR_ETH0" \
+    -e "/Bind/c Bind $ADDR_TUN0" \
+    $PROXY_CONFIG_FILE
 
 if [[ $HTTP_PROXY_USERNAME && $HTTP_PROXY_PASSWORD ]]; then
-    echo 'info: starting http proxy with credentials'
-    printf 'BasicAuth %s %s\n' "$HTTP_PROXY_USERNAME" "$HTTP_PROXY_PASSWORD" >> $proxy_config_file
+    log_msg 'info: starting http proxy with credentials'
+    printf 'BasicAuth %s %s\n' "$HTTP_PROXY_USERNAME" "$HTTP_PROXY_PASSWORD" >> $PROXY_CONFIG_FILE
 elif [[ -f "/run/secrets/$HTTP_PROXY_USERNAME_SECRET" && -f "/run/secrets/$HTTP_PROXY_PASSWORD_SECRET" ]]; then
-    echo 'info: starting http proxy with credentials'
+    log_msg 'info: starting http proxy with credentials'
     printf 'BasicAuth %s %s\n' \
         "$(cat /run/secrets/"$HTTP_PROXY_USERNAME_SECRET")" \
-        "$(cat /run/secrets/"$HTTP_PROXY_PASSWORD_SECRET")" >> $proxy_config_file
+        "$(cat /run/secrets/"$HTTP_PROXY_PASSWORD_SECRET")" >> $PROXY_CONFIG_FILE
 else
-    echo 'info: starting http proxy without credentials'
+    log_msg 'info: starting http proxy without credentials'
 fi
 
-exec tinyproxy -d -c $proxy_config_file
+exec tinyproxy -d -c $PROXY_CONFIG_FILE
+
+# Never executed
+exit $?
