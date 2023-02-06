cdc257b59be8add47ec637852b1f2b0d21d64d4b
Pablo Sanchez <pablo@blueoakdb.com>
2023-02-06 11:06:27 -0500

    Revert mixed variable case.  Implemented the proper check for the .ovpn file:  exist and non-zero length.

--- data/scripts/entry.sh
+++ data/scripts/entry.sh
@@ -2,24 +2,24 @@
 
 set -e
 
-BASENAME=$(basename "$0")
+basename=$(basename "$0")
 log_msg() {
-   echo -e "$(date +'%F %T')" ["$BASENAME"] "$@"
+   echo -e "$(date +'%F %T')" ["$basename"] "$@"
 }
 
 cleanup() {
-   if [[ -n "$OPENVPN_CHILD" ]]; then
+   if [[ -n "$openvpn_child" ]]; then
       log_msg "Asking openvpn to exit gracefully"
-      kill SIGTERM $OPENVPN_CHILD > /dev/null 2>&1
-      I=15
-      while [[ $I -ge 0 ]] ; do
-         kill -0 $OPENVPN_CHILD > /dev/null 2>&1
+      kill SIGTERM $openvpn_child > /dev/null 2>&1
+      i=15
+      while [[ $i -ge 0 ]] ; do
+         kill -0 $openvpn_child > /dev/null 2>&1
          if [[ $? -eq 1 ]] ; then
             break
          fi
-         log_msg "Waiting on openvpn to end ... $I"
+         log_msg "Waiting on openvpn to end ... $i"
          sleep 1
-         ((I--))
+         ((i--))
       done
    fi
 }
@@ -46,7 +46,7 @@ dump_user_settings() {
    log_msg "Server poll timeout (--server-poll-timeout): ${SERVER_POLL:-120}"
    log_msg "Ping (--ping): ${PING:-15}"
    log_msg "Ping restart (--ping-restart): ${PING_RESTART:-120}"
-   log_msg "When kill switch enabled, dig timeout:  ${DIG_TIMEOUT:-5}"
+   log_msg "When kill switch enabled, dig timeout: ${DIG_TIMEOUT:-5}"
    log_msg "Using OpenVPN log level: $VPN_LOG_LEVEL"
    if [[ -n "$TAP" ]] ; then
       log_msg "Container script 'eth0' remapped to '${TAP}'"
@@ -90,40 +90,40 @@ dump_user_settings() {
 
 gen_working_VPN_file() {
    if [[ $VPN_CONFIG_FILE ]]; then
-      ORIGINAL_CONFIG_FILE=vpn/$VPN_CONFIG_FILE
+      original_config_file=vpn/$VPN_CONFIG_FILE
    elif [[ $VPN_CONFIG_PATTERN ]]; then
-      ORIGINAL_CONFIG_FILE=$(find vpn -name "$VPN_CONFIG_PATTERN" 2> /dev/null | sort | shuf -n 1)
+      original_config_file=$(find vpn -name "$VPN_CONFIG_PATTERN" 2> /dev/null | sort | shuf -n 1)
    else
-      ORIGINAL_CONFIG_FILE=$(find vpn -name '*.conf' -o -name '*.ovpn' 2> /dev/null | sort | shuf -n 1)
+      original_config_file=$(find vpn -name '*.conf' -o -name '*.ovpn' 2> /dev/null | sort | shuf -n 1)
    fi
 
-   if [[ ! -f $ORIGINAL_CONFIG_FILE ]]; then
-      >&2 log_msg "erro: no vpn configuration file found"
+   if [[ ! -s $original_config_file ]]; then
+      >&2 log_msg "erro: no vpn configuration file found or zero-sized"
       exit 1
    fi
 
-   log_msg "info: original configuration file: $ORIGINAL_CONFIG_FILE"
+   log_msg "info: original configuration file: $original_config_file"
 
    # Create a new configuration file to modify so the original is left
    # untouched.
    #
    # Use the passed in $DEBUG_VPN_CONFIG_FILE variable to determine whether
    # the modified file is ephemeral or not.
-   MOD_DIR="/tmp"
+   mod_dir="/tmp"
    if is_enabled "$DEBUG_VPN_CONFIG_FILE" ; then
-      MOD_DIR="vpn"
+      mod_dir="vpn"
    fi
-   MODIFIED_CONFIG_FILE=$MOD_DIR/openvpn.$(tr -dc A-Za-z0-9 </dev/urandom | head -c8).conf
+   modified_config_file=$mod_dir/openvpn.$(tr -dc A-Za-z0-9 </dev/urandom | head -c8).conf
 
-   log_msg "info: modified configuration file: $MODIFIED_CONFIG_FILE"
-   grep -Ev '(^up\s|^down\s)' "$ORIGINAL_CONFIG_FILE" > "$MODIFIED_CONFIG_FILE"
+   log_msg "info: modified configuration file: $modified_config_file"
+   grep -Ev '(^up\s|^down\s)' "$original_config_file" > "$modified_config_file"
 
    # Remove carriage returns (\r) from the config file
-   sed -i 's/\r$//g' "$MODIFIED_CONFIG_FILE"
+   sed -i 's/\r$//g' "$modified_config_file"
 }
 
 setup_kill_switch() {
-   DEFAULT_GATEWAY=$(ip -4 route | grep 'default via' | awk '{print $3}')
+   default_gateway=$(ip -4 route | grep 'default via' | awk '{print $3}')
    case "$KILL_SWITCH" in
       'iptables')
          log_msg "Configuring $KILL_SWITCH kill switch"
@@ -132,43 +132,43 @@ setup_kill_switch() {
          iptables -A INPUT -i lo -j ACCEPT
          iptables -A OUTPUT -o lo -j ACCEPT
 
-         LOCAL_SUBNET=$(ip -4 route | grep 'scope link' | awk '{print $1}')
-         iptables -A INPUT -s "$LOCAL_SUBNET" -j ACCEPT
-         iptables -A OUTPUT -d "$LOCAL_SUBNET" -j ACCEPT
+         local_subnet=$(ip -4 route | grep 'scope link' | awk '{print $1}')
+         iptables -A INPUT -s "$local_subnet" -j ACCEPT
+         iptables -A OUTPUT -d "$local_subnet" -j ACCEPT
 
          if [[ $SUBNETS ]]; then
-            for SUBNET in ${SUBNETS//,/ }; do
-               ip route add "$SUBNET" via "$DEFAULT_GATEWAY" dev "${TAP:-eth0}"
-               iptables -A INPUT -s "$SUBNET" -j ACCEPT
-               iptables -A OUTPUT -d "$SUBNET" -j ACCEPT
+            for subnet in ${SUBNETS//,/ }; do
+               ip route add "$subnet" via "$default_gateway" dev "${TAP:-eth0}"
+               iptables -A INPUT -s "$subnet" -j ACCEPT
+               iptables -A OUTPUT -d "$subnet" -j ACCEPT
             done
          fi
 
-         GLOBAL_PORT=$(grep "^port " "$MODIFIED_CONFIG_FILE" | awk '{print $2}')
-         GLOBAL_PROTOCOL=$(grep "^proto " "$MODIFIED_CONFIG_FILE" | awk '{print $2}')  # {$2 = substr($2, 1, 3)} 2
-         REMOTES=$(grep "^remote " "$MODIFIED_CONFIG_FILE" | awk '{print $2, $3, $4}')
-         IP_REGEX='^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$'
-         while IFS= read -r LINE; do
-            IFS=' ' read -ra REMOTE <<< "$LINE"
-            ADDRESS=${REMOTE[0]}
-            PORT=${REMOTE[1]:-${GLOBAL_PORT:-1194}}
-            PROTOCOL=${REMOTE[2]:-${GLOBAL_PROTOCOL:-udp}}
+         global_port=$(grep "^port " "$modified_config_file" | awk '{print $2}')
+         global_protocol=$(grep "^proto " "$modified_config_file" | awk '{print $2}')  # {$2 = substr($2, 1, 3)} 2
+         remotes=$(grep "^remote " "$modified_config_file" | awk '{print $2, $3, $4}')
+         ip_regex='^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$'
+         while IFS= read -r line; do
+            IFS=' ' read -ra remote <<< "$line"
+            address=${remote[0]}
+            port=${remote[1]:-${global_port:-1194}}
+            protocol=${remote[2]:-${global_protocol:-udp}}
 
-            if [[ $ADDRESS =~ $IP_REGEX ]]; then
-               iptables -A OUTPUT -o "${TAP:-eth0}" -d "$ADDRESS" -p "$PROTOCOL" --dport "$PORT" -j ACCEPT
+            if [[ $address =~ $ip_regex ]]; then
+               iptables -A OUTPUT -o "${TAP:-eth0}" -d "$address" -p "$protocol" --dport "$port" -j ACCEPT
             else
-               log_msg "dig lookup on $ADDRESS"
-               for IP in $(dig -4 +timeout="${DIG_TIMEOUT:-5}" +tries=1 +short "$ADDRESS"); do
-                  if echo "$IP" | grep -s '^[0-9]' > /dev/null 2>&1 ; then
-                     iptables -A OUTPUT -o "${TAP:-eth0}" -d "$IP" -p "$PROTOCOL" --dport "$PORT" -j ACCEPT
-                     printf "%s %s\n" "$IP" "$ADDRESS" >> /etc/hosts
+               log_msg "dig lookup on $address"
+               for ip in $(dig -4 +timeout="${DIG_TIMEOUT:-5}" +tries=1 +short "$address"); do
+                  if echo "$ip" | grep -s '^[0-9]' > /dev/null 2>&1 ; then
+                     iptables -A OUTPUT -o "${TAP:-eth0}" -d "$ip" -p "$protocol" --dport "$port" -j ACCEPT
+                     printf "%s %s\n" "$ip" "$address" >> /etc/hosts
                   else
                      log_msg "dig returned malformed IP ($IP).  Cannot recover. Bye."
                      exit 1
                   fi
                done
             fi
-         done <<< "$REMOTES"
+         done <<< "$remotes"
          iptables -A INPUT -i tun0 -j ACCEPT
          iptables -A OUTPUT -o tun0 -j ACCEPT
          iptables -P INPUT DROP
@@ -179,7 +179,7 @@ setup_kill_switch() {
       'nftables')
          log_msg "Configuring $KILL_SWITCH kill switch"
 
-         NFTABLES_CONFIG_FILE=config/nftables.conf
+         nftables_config_file=config/nftables.conf
 
          printf '%s\n' \
                 '#!/usr/bin/nft' '' \
@@ -191,65 +191,65 @@ setup_kill_switch() {
                 'add rule inet killswitch incoming iifname lo accept' '' \
                 'add chain inet killswitch outgoing { type filter hook output priority 0; policy drop; }' \
                 'add rule inet killswitch outgoing ct state established,related accept' \
-                'add rule inet killswitch outgoing oifname lo accept' '' > $NFTABLES_CONFIG_FILE
+                'add rule inet killswitch outgoing oifname lo accept' '' > $nftables_config_file
 
-         LOCAL_SUBNET=$(ip -4 route | grep 'scope link' | awk '{print $1}')
+         local_subnet=$(ip -4 route | grep 'scope link' | awk '{print $1}')
          printf '%s\n' \
                 '# allow traffic to/from the Docker subnet' \
-                "add rule inet killswitch incoming ip saddr $LOCAL_SUBNET accept" \
-                "add rule inet killswitch outgoing ip daddr $LOCAL_SUBNET accept" '' >> $NFTABLES_CONFIG_FILE
+                "add rule inet killswitch incoming ip saddr $local_subnet accept" \
+                "add rule inet killswitch outgoing ip daddr $local_subnet accept" '' >> $nftables_config_file
 
          if [[ $SUBNETS ]]; then
-            printf '# allow traffic to/from the specified subnets\n' >> $NFTABLES_CONFIG_FILE
+            printf '# allow traffic to/from the specified subnets\n' >> $nftables_config_file
             for subnet in ${SUBNETS//,/ }; do
-               ip route add "$subnet" via "$DEFAULT_GATEWAY" dev "${TAP:-eth0}"
+               ip route add "$subnet" via "$default_gateway" dev "${TAP:-eth0}"
                printf '%s\n' \
                       "add rule inet killswitch incoming ip saddr $subnet accept" \
-                      "add rule inet killswitch outgoing ip daddr $subnet accept" '' >> $NFTABLES_CONFIG_FILE
+                      "add rule inet killswitch outgoing ip daddr $subnet accept" '' >> $nftables_config_file
             done
          fi
 
-         GLOBAL_PORT=$(grep "^port " "$MODIFIED_CONFIG_FILE" | awk '{print $2}')
-         GLOBAL_PROTOCOL=$(grep "^proto " "$MODIFIED_CONFIG_FILE" | awk '{print $2}')  # {$2 = substr($2, 1, 3)} 2
-         REMOTES=$(grep "^remote " "$MODIFIED_CONFIG_FILE" | awk '{print $2, $3, $4}')
+         global_port=$(grep "^port " "$modified_config_file" | awk '{print $2}')
+         global_protocol=$(grep "^proto " "$modified_config_file" | awk '{print $2}')  # {$2 = substr($2, 1, 3)} 2
+         remotes=$(grep "^remote " "$modified_config_file" | awk '{print $2, $3, $4}')
 
-         printf '# allow traffic to the VPN server(s)\n' >> $NFTABLES_CONFIG_FILE
-         IP_REGEX='^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$'
-         while IFS= read -r LINE; do
-            IFS=' ' read -ra REMOTE <<< "$LINE"
-            ADDRESS=${REMOTE[0]}
-            PORT=${REMOTE[1]:-${GLOBAL_PORT:-1194}}
-            PROTOCOL=${REMOTE[2]:-${GLOBAL_PROTOCOL:-udp}}
+         printf '# allow traffic to the VPN server(s)\n' >> $nftables_config_file
+         ip_regex='^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$'
+         while IFS= read -r line; do
+            IFS=' ' read -ra remote <<< "$line"
+            address=${remote[0]}
+            port=${remote[1]:-${global_port:-1194}}
+            protocol=${remote[2]:-${global_protocol:-udp}}
 
-            if [[ $ADDRESS =~ $IP_REGEX ]]; then
+            if [[ $address =~ $ip_regex ]]; then
                printf '%s\n' \
-                      "add rule inet killswitch outgoing oifname ${TAP:-eth0} ip daddr $ADDRESS $PROTOCOL dport $PORT accept" >> $NFTABLES_CONFIG_FILE
+                      "add rule inet killswitch outgoing oifname ${TAP:-eth0} ip daddr $address $protocol dport $port accept" >> $nftables_config_file
             else
-               log_msg "dig lookup on $ADDRESS"
-               for IP in $(dig -4 +timeout="${DIG_TIMEOUT:-5}" +tries=1 +short "$ADDRESS"); do
-                  if echo "$IP" | grep -s '^[0-9]' > /dev/null 2>&1 ; then
+               log_msg "dig lookup on $address"
+               for ip in $(dig -4 +timeout="${DIG_TIMEOUT:-5}" +tries=1 +short "$address"); do
+                  if echo "$ip" | grep -s '^[0-9]' > /dev/null 2>&1 ; then
                      printf '%s\n' \
-                            "add rule inet killswitch outgoing oifname ${TAP:-eth0} ip daddr $IP $PROTOCOL dport $PORT accept" >> $NFTABLES_CONFIG_FILE
-                     printf "%s %s\n" "$IP" "$ADDRESS" >> /etc/hosts
+                            "add rule inet killswitch outgoing oifname ${TAP:-eth0} ip daddr $ip $protocol dport $port accept" >> $nftables_config_file
+                     printf "%s %s\n" "$ip" "$address" >> /etc/hosts
                   else
-                     log_msg "dig returned malformed IP ($IP).  Cannot recover.  Bye."
+                     log_msg "dig returned malformed IP ($ip).  Cannot recover.  Bye."
                      exit 1
                   fi
                done
             fi
-         done <<< "$REMOTES"
+         done <<< "$remotes"
 
          printf '%s\n' \
                 '' '# allow traffic over the VPN interface' \
                 "add rule inet killswitch incoming iifname tun0 accept" \
-                "add rule inet killswitch outgoing oifname tun0 accept" >> $NFTABLES_CONFIG_FILE
+                "add rule inet killswitch outgoing oifname tun0 accept" >> $nftables_config_file
 
-         nft -f $NFTABLES_CONFIG_FILE
+         nft -f $nftables_config_file
          ;;
       *)
          log_msg "info: kill switch is off"
          for subnet in ${SUBNETS//,/ }; do
-            ip route add "$subnet" via "$DEFAULT_GATEWAY" dev "${TAP:-eth0}"
+            ip route add "$subnet" via "$default_gateway" dev "${TAP:-eth0}"
          done
          ;;
    esac
@@ -265,8 +265,8 @@ mkdir -p /data/{config,scripts,vpn}
 log_msg "Devices"
 log_msg "======="
 
-ip --brief a | while read -r DEV ; do
-   log_msg "$DEV"
+ip --brief a | while read -r dev ; do
+   log_msg "$dev"
 done
 log_msg
 
@@ -292,8 +292,8 @@ fi
 trap 'log_msg "Caught container exit signal"; cleanup; log_msg "Bye."; exit 0' SIGTERM
 
 log_msg "Backgrounding openvpn ..."
-OPENVPN_ARGS=(
-   "--config" "$MODIFIED_CONFIG_FILE"
+openvpn_args=(
+   "--config" "$modified_config_file"
    "--auth-nocache"
    "--cd" "vpn"
    "--pull-filter" "ignore" "ifconfig-ipv6 "
@@ -311,38 +311,38 @@ OPENVPN_ARGS=(
 )
 
 if is_enabled "$USE_FAST_IO" ; then
-   OPENVPN_ARGS+=(
+   openvpn_args+=(
       "--fast-io"
    )
 fi
 
 if [[ -n "$SNDBUF" ]] ; then
-   OPENVPN_ARGS+=(
+   openvpn_args+=(
       "--sndbuf" "${SNDBUF}"
    )
 fi
 
 if [[ -n "$RCVBUF" ]] ; then
-   OPENVPN_ARGS+=(
+   openvpn_args+=(
       "--rcvbuf" "${RCVBUF}"
    )
 fi
 
 if is_enabled "$USE_VPN_DNS" ; then
-   OPENVPN_ARGS+=(
+   openvpn_args+=(
       "--up" "/etc/openvpn/up.sh"
       "--down" "/etc/openvpn/down.sh"
    )
 fi
 
 if [[ $VPN_AUTH_SECRET ]]; then
-   OPENVPN_ARGS+=("--auth-user-pass" "/run/secrets/$VPN_AUTH_SECRET")
+   openvpn_args+=("--auth-user-pass" "/run/secrets/$VPN_AUTH_SECRET")
 fi
 
-openvpn "${OPENVPN_ARGS[@]}" &
-OPENVPN_CHILD=$!
+openvpn "${openvpn_args[@]}" &
+openvpn_child=$!
 
-wait $OPENVPN_CHILD
+wait $openvpn_child
 
 # Never reached
 cleanup
