b5551a22abada64cf2ff2cc54828c7d5e74fff27
Pablo Sanchez <pablo@blueoakdb.com>
2023-02-06 11:06:22 -0500

    revert mixed variable case

--- data/scripts/run-http-proxy.sh
+++ data/scripts/run-http-proxy.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-BASENAME=$(basename "$0")
+basename=$(basename "$0")
 log_msg() {
-   echo -e "$(date +'%F %T')" ["$BASENAME"] "$@"
+   echo -e "$(date +'%F %T')" ["$basename"] "$@"
 }
 
 log_msg "Waiting for tun0 to become available ..."
@@ -13,28 +13,28 @@ until ip link show tun0 2>&1 | grep -qv "does not exist"; do
 done
 log_msg "tun0 ready."
 
-PROXY_CONFIG_FILE=config/http-proxy.conf
+proxy_config_file=config/http-proxy.conf
 
-ADDR_ETH0=$(ip address show "${TAP:-eth0}" | grep 'inet ' | awk '{split($2, inet, "/"); print inet[1]}')
-ADDR_TUN0=$(ip address show tun0 | grep 'inet ' | awk '{split($2, inet, "/"); print inet[1]}')
+addr_eth0=$(ip address show "${TAP:-eth0}" | grep 'inet ' | awk '{split($2, inet, "/"); print inet[1]}')
+addr_tun0=$(ip address show tun0 | grep 'inet ' | awk '{split($2, inet, "/"); print inet[1]}')
 sed -i \
-    -e "/Listen/c Listen $ADDR_ETH0" \
-    -e "/Bind/c Bind $ADDR_TUN0" \
-    $PROXY_CONFIG_FILE
+    -e "/Listen/c Listen $addr_eth0" \
+    -e "/Bind/c Bind $addr_tun0" \
+    $proxy_config_file
 
 if [[ $HTTP_PROXY_USERNAME && $HTTP_PROXY_PASSWORD ]]; then
     log_msg 'info: starting http proxy with credentials'
-    printf 'BasicAuth %s %s\n' "$HTTP_PROXY_USERNAME" "$HTTP_PROXY_PASSWORD" >> $PROXY_CONFIG_FILE
+    printf 'BasicAuth %s %s\n' "$HTTP_PROXY_USERNAME" "$HTTP_PROXY_PASSWORD" >> $proxy_config_file
 elif [[ -f "/run/secrets/$HTTP_PROXY_USERNAME_SECRET" && -f "/run/secrets/$HTTP_PROXY_PASSWORD_SECRET" ]]; then
     log_msg 'info: starting http proxy with credentials'
     printf 'BasicAuth %s %s\n' \
         "$(cat /run/secrets/"$HTTP_PROXY_USERNAME_SECRET")" \
-        "$(cat /run/secrets/"$HTTP_PROXY_PASSWORD_SECRET")" >> $PROXY_CONFIG_FILE
+        "$(cat /run/secrets/"$HTTP_PROXY_PASSWORD_SECRET")" >> $proxy_config_file
 else
     log_msg 'info: starting http proxy without credentials'
 fi
 
-exec tinyproxy -d -c $PROXY_CONFIG_FILE
+exec tinyproxy -d -c $proxy_config_file
 
 # Never executed
 exit $?
--- data/scripts/run-socks-proxy.sh
+++ data/scripts/run-socks-proxy.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-BASENAME=$(basename "$0")
+basename=$(basename "$0")
 log_msg() {
-   echo -e "$(date +'%F %T')" ["$BASENAME"] "$@"
+   echo -e "$(date +'%F %T')" ["$basename"] "$@"
 }
 
 log_msg "Waiting for tun0 to become available ..."
@@ -13,24 +13,24 @@ until ip link show tun0 2>&1 | grep -qv "does not exist"; do
 done
 log_msg "tun0 ready."
 
-PROXY_CONFIG_FILE=config/socks-proxy.conf
+proxy_config_file=config/socks-proxy.conf
 
 if [[ $SOCKS_LISTEN_ON ]]; then
-    sed -i "/internal: /c internal: $SOCKS_LISTEN_ON port = 1080" $PROXY_CONFIG_FILE
+    sed -i "/internal: /c internal: $SOCKS_LISTEN_ON port = 1080" $proxy_config_file
 fi
 if [[ $SOCKS_PROXY_USERNAME && $SOCKS_PROXY_PASSWORD ]]; then
     log_msg 'info: starting socks proxy with credentials'
     useradd "$SOCKS_PROXY_USERNAME" -s /bin/false -M -p "$(mkpasswd "$SOCKS_PROXY_PASSWORD")"
-    sed -i "/method: /c method: username" $PROXY_CONFIG_FILE
+    sed -i "/method: /c method: username" $proxy_config_file
 elif [[ -f "/run/secrets/$SOCKS_PROXY_USERNAME_SECRET" && -f "/run/secrets/$SOCKS_PROXY_PASSWORD_SECRET" ]]; then
     log_msg 'info: starting socks proxy with credentials'
     useradd "$(cat /run/secrets/"$SOCKS_PROXY_USERNAME_SECRET")" -s /bin/false -M -p "$(mkpasswd "$(cat /run/secrets/"$SOCKS_PROXY_PASSWORD_SECRET")")"
-    sed -i "/method: /c method: username" $PROXY_CONFIG_FILE
+    sed -i "/method: /c method: username" $proxy_config_file
 else
     log_msg 'info: starting socks proxy without credentials'
 fi
 
-exec sockd -f $PROXY_CONFIG_FILE
+exec sockd -f $proxy_config_file
 
 # Never executed
 exit $?
